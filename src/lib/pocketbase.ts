b48a6ba750a0b5df41d88b6f417e1c1bc2c2f655
Sistemasc2tzom <sistemasc5itlax@gmail.com>
2026-04-08 16:23:08 -0600

    chore: enable environment-based PocketBase URL for deployment

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const PB_URL = 'http://127.0.0.1:8090';
+const PB_URL = import.meta.env.VITE_PB_URL || 'http://127.0.0.1:8090';
 
 export const pb = new PocketBase(PB_URL);
 
